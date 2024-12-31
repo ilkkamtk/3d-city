30fd94a64eaad69fdca28b4302f91d9d991f3c64
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2024-12-31 14:54:09 +0200

    Optimize matrix handling in Trees component by using refs for transformations to enhance performance and reduce memory allocation

--- src/components/Trees.tsx
+++ src/components/Trees.tsx
@@ -1,11 +1,16 @@
 import { Mesh, InstancedMesh, Matrix4 } from 'three';
 import { useMapContext } from '../hooks/ContextHooks';
 import { useFBX } from '@react-three/drei';
-import { useMemo } from 'react';
+import { useMemo, useRef } from 'react';
 
 const Trees = () => {
   const { trees } = useMapContext();
   const treeFBX = useFBX('tree3.fbx');
+  // Set position and rotation for each instance
+  const matrix = useRef(new Matrix4());
+  const rotationX = useRef(new Matrix4());
+  const rotationZ = useRef(new Matrix4());
+  const scale = useRef(new Matrix4());
 
   // Find the first mesh in the FBX to use as template
   const templateMesh = useMemo((): Mesh | null => {
@@ -33,30 +38,31 @@ const Trees = () => {
     instancedMesh.castShadow = true;
     instancedMesh.receiveShadow = true;
 
-    // Set position and rotation for each instance
-    const matrix = new Matrix4();
-    const rotationX = new Matrix4();
-    const rotationZ = new Matrix4();
-    const scale = new Matrix4();
-
     trees.forEach((tree, i) => {
       // Reset the matrix for each tree
-      matrix.identity();
+      matrix.current.identity();
 
       // Set the position
-      matrix.setPosition(tree.x, 0, tree.y);
+      matrix.current.setPosition(tree.x, 0, tree.y);
 
       // X-axis rotation to make the tree stand up
-      rotationX.makeRotationX(-Math.PI / 2);
-      // randomize the  Y-axis rotation
-      rotationZ.makeRotationZ(Math.random() * Math.PI * 2);
+      rotationX.current.makeRotationX(-Math.PI / 2);
+      // randomize the  Z-axis rotation
+      rotationZ.current.makeRotationZ(Math.random() * Math.PI * 2);
       // scale the tree
-      scale.makeScale(tree.height / 5, tree.height / 5, tree.height / 5);
+      scale.current.makeScale(
+        tree.height / 5,
+        tree.height / 5,
+        tree.height / 5,
+      );
 
-      matrix.multiply(rotationX).multiply(rotationZ).multiply(scale);
+      matrix.current
+        .multiply(rotationX.current)
+        .multiply(rotationZ.current)
+        .multiply(scale.current);
 
       // Add the transformed matrix to the instanced mesh
-      instancedMesh.setMatrixAt(i, matrix);
+      instancedMesh.setMatrixAt(i, matrix.current);
     });
 
     instancedMesh.instanceMatrix.needsUpdate = true;
